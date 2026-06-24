00267cc49769055cecc34f8e1783213f77654057
abdulaziz27 <doelziez212@gmail.com>
2026-06-24 22:28:18 +0700

    Fix: move swagger to /api/docs for Vercel routing

--- api/swagger.ts
+++ api/swagger.ts
@@ -37,5 +37,5 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 export const setupSwagger = (app: Application) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
